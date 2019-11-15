5b8c4c0efa7564e15316c69bdff5ae1b201e4c9d
JokuPelle <juuso.ylikoski@student.lut.fi>
2019-11-15 23:37:16 +0200

    backend edit

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,2 +1,32 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+var DataSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    ean: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    usage: {
+        type: Number,
+        required: false
+    },
+    co2: {
+        type: Number,
+        required: false
+    },
+    photo: {
+        type: String,
+        required: false
+    }
+});
+
+const DataModel = mongoose.model("DataModel", DataSchema);
+module.exports = DataModel;
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 app.use(express.json());
 
+const Data = require("./models/product");
+
 const mongoUI = "mongodb+srv://spend:wise@spendwisedata-gaqmj.mongodb.net/Datas";
 mongoose.connect(mongoUI, {useNewUrlParser: true, useUnifiedTopology: true},(err) => {
     if (err) console.log(err);
@@ -13,15 +15,19 @@ mongoose.connect(mongoUI, {useNewUrlParser: true, useUnifiedTopology: true},(err
     }
 });
 
+app.post("/newdata", (req, res) => {
+    console.log("yolo");
+})
+
 app.post("/getinfo", (req, res) => {
     const data = res.json(req.body);
-    console.log(data.body.name);
-    res.status(200).send(res);
+    console.log(data.body);
+    res.status(204);
 });
 
 app.get("/", (req, res) => {
     console.log("homepage");
-    res.send("hello there");
+    res.send("hello ttttthere");
 });
 
 app.listen(port, () => console.log(`Listening to port number ${port}`));
