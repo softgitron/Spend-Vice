726575b1e8b2c7a22d6b483df1514d44dfea537f
JokuPelle <juuso.ylikoski@student.lut.fi>
2019-11-17 00:34:16 +0200

    dummy update

--- backend/dummy.py
+++ backend/dummy.py
@@ -6,9 +6,9 @@ givenURL = sys.argv[1]
 #Save the generated ean to "generatedEAN" and print it!
 #generatedEAN = 430303030
 x = {
-    "Price":300,
-    "Image":"https://images.unsplash.com/photo-1504148455328-c376907d081c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1143&q=80",
-    "Name":"Epic Product"
+    "Price":420,
+    "Image":"https://image.businessinsider.com/59974812b0e0b595758b5449?width=1100&format=jpeg&auto=webp",
+    "Name":"Essential Phone"
 }
 y = json.dumps(x)
 print(y)
