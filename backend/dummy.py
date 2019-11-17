32b962ed0e84a2876b8c0089516c1ccf6819b4d4
JokuPelle <juuso.ylikoski@student.lut.fi>
2019-11-17 09:15:29 +0200

    small links

--- backend/dummy.py
+++ backend/dummy.py
@@ -5,19 +5,19 @@ import json
 givenURL = sys.argv[1]
 #Save the generated ean to "generatedEAN" and print it!
 #generatedEAN = 430303030
-if (givenURL == "https://www.amazon.com/Bose-QuietComfort-Wireless-Headphones-Cancelling/dp/B0756CYWWD?_encoding=UTF8&%2AVersion%2A=1&%2Aentries%2A=0"):
+if (givenURL == "https://www.amazon.com/Bose-QuietComfort-Wireless-Headphones-Cancelling/dp/B0756CYWWD?_encoding=UTF8&%2AVersion%2A=1&%2Aentries%2A=0" or givenURL == "https://www.amazon.com/Bose-QuietComfort-Wireless-Headphones-Cancelling/dp/B0756CYWWD"):
     x = {
         "Price": 349,
         "Image":"https://images-na.ssl-images-amazon.com/images/I/51sAtKgDkDL.jpg",
         "Name":"Bose Quite Comfort Headphones"
     }
-elif (givenURL == "https://www.amazon.com/Sandisk-128GB-Flash-Memory-Drive/dp/B00P8XQPY4/ref=sr_1_27?qid=1573973513&sr=8-27&srs=18332380011"):
+elif (givenURL == "https://www.amazon.com/Sandisk-128GB-Flash-Memory-Drive/dp/B00P8XQPY4/ref=sr_1_27?qid=1573973513&sr=8-27&srs=18332380011" or givenURL == "https://www.amazon.com/Sandisk-128GB-Flash-Memory-Drive/dp/B00P8XQPY4"):
     x = {
         "Price":18.92,
         "Image":"https://images-na.ssl-images-amazon.com/images/I/51dudPA-u4L._SL1100_.jpg",
         "Name":"Sandisk Flash Memory Drive"
     }
-elif (givenURL == "https://www.amazon.com/AmazonBasics-Hardside-Spinner-Luggage-20-Inch/dp/B071VG5N9D?pf_rd_p=8eb011f8-a9f1-4c31-a254-7a5f8477042c&pd_rd_wg=XWO5U&pf_rd_r=66M6HZ2P9QTK992J5CXE&ref_=pd_gw_unk&pd_rd_w=Potrc&pd_rd_r=87982f33-2aad-465a-85ca-70104eac5d22"):
+elif (givenURL == "https://www.amazon.com/AmazonBasics-Hardside-Spinner-Luggage-20-Inch/dp/B071VG5N9D?pf_rd_p=8eb011f8-a9f1-4c31-a254-7a5f8477042c&pd_rd_wg=XWO5U&pf_rd_r=66M6HZ2P9QTK992J5CXE&ref_=pd_gw_unk&pd_rd_w=Potrc&pd_rd_r=87982f33-2aad-465a-85ca-70104eac5d22" or givenURL == "https://www.amazon.com/Sandisk-128GB-Flash-Memory-Drive/dp/B00P8XQPY4"):
     x = {
         "Price":49.99,
         "Image":"https://images-na.ssl-images-amazon.com/images/I/91SJsAkdZDL._UX679_.jpg",
